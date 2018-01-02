581900f4253eda714800be3c21186257104a934e
Miroslav Semerad <miretz@gmail.com>
2018-01-02 11:01:14 +0100

    Expose URL

--- src/main/java/com/miretz/textextract/Main.java
+++ src/main/java/com/miretz/textextract/Main.java
@@ -14,7 +14,7 @@ import java.net.URI;
 
 public class Main {
 
-    public static final String BASE_URI = "http://localhost:8080/api/";
+    public static final String BASE_URI = "http://0.0.0.0:8080/api/";
 
     public static HttpServer startServer() {
         final ResourceConfig rc = new ResourceConfig().packages("com.miretz.textextract");
